c402e737cef535b8a544b32ed26bd78da47a3384
Keanu Chau <kechau@ucdavis.edu>
2024-12-13 19:02:54 -0800

    Fix camera to fill screen

--- the-shadow-that-follows/scripts/start_screen.gd
+++ the-shadow-that-follows/scripts/start_screen.gd
@@ -2,11 +2,12 @@ extends Control
 
 @onready var menu_music = $Sound/MenusMusic
 @onready var button_hover: AudioStreamPlayer2D = $Sound/ButtonHover
+@onready var camera_2d: Camera2D = $Camera2D
 
 
 func _ready() -> void:
 	menu_music.play()
-
+	
 func _on_start_game_button_down() -> void:
 	get_tree().change_scene_to_file("res://scenes/story_screen.tscn")
 	
